6a267f70c78478e6b652bc4368966300d002d773
Fayecodeur <fayecodeur99@gmail.com>
2025-03-02 16:41:12 +0000

    composant pour les items de la navigation

---
+++ src/assets/components/HeaderItems.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function HeaderItems({ name, Icon }) {
+  return (
+    <button className="flex items-center gap-2 text-white hover:underline underline-offset-8">
+      <Icon />
+      <p>{name}</p>
+    </button>
+  );
+}
