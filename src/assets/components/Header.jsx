1c25f2735dfd81dbfbd2a13acf99b8b90e8f9bb2
Fayecodeur <fayecodeur99@gmail.com>
2025-03-02 16:10:10 +0000

    création du fichier components

---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,8 @@
+import { FaHome, FaPlus, FaSearch, FaStar } from "react-icons/fa";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { PiTelevisionBold } from "react-icons/pi";
+import { RiMovie2Line } from "react-icons/ri";
+import Logo from "../Images/disney-logo.png";
+export default function Header() {
+  return <div>Header</div>;
+}
