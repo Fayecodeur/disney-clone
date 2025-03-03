1f00b75b3bafc1f686df4653d7b3766b80ca4f8d
Fayecodeur <fayecodeur99@gmail.com>
2025-03-03 13:49:05 +0000

    inclusion du composant Categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import Categories from "./assets/components/Categories.jsx";
 import Header from "./assets/components/Header.jsx";
+import Slider from "./assets/components/Slider.jsx";
 
 function App() {
   return (
     <>
       <Header />
+      <Slider />
+      <Categories />
     </>
   );
 }
