808edef24aafd266297bb5f519a195cf1a8d9a52
Fayecodeur <fayecodeur99@gmail.com>
2025-03-04 18:27:10 +0000

    changement de l'api key

--- src/Services/CallApi.jsx
+++ src/Services/CallApi.jsx
@@ -1,16 +1,14 @@
 import axios from "axios";
-
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const movieBaseUrl = "https://api.themoviedb.org/3";
-const api_key = "11937915d20a080aeb5761f924ad73f5";
 
 // https://api.themoviedb.org/3/trending/all/day?api_key=0c9a6f18917ec1bb52e881f5a3ca8db0
 
 const getVideos = axios.get(
-  `${movieBaseUrl}/trending/all/day?api_key=${api_key}`
+  `${movieBaseUrl}/trending/all/day?api_key=${API_KEY}`
 );
 
-const movieByGenreBaseURL =
-  "https://api.themoviedb.org/3/discover/movie?api_key=11937915d20a080aeb5761f924ad73f5";
+const movieByGenreBaseURL = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
 
 const getMovieById = (id) =>
   axios.get(`${movieByGenreBaseURL}&with_genres=${id}`);
