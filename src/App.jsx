ad30ae7ce7d51c7fa139cf1ecddba4368e326fe2
Fayecodeur <fayecodeur99@gmail.com>
2025-03-04 16:15:42 +0000

    import de composants

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import "./App.css";
 import Categories from "./components/Categories.jsx";
 import Header from "./components/Header.jsx";
 import Slider from "./components/Slider.jsx";
-
+import GenreListMoovies from "./components/GenreListMoovies";
 function App() {
   return (
     <>
       <Header />
       <Slider />
       <Categories />
+      <GenreListMoovies />
     </>
   );
 }
