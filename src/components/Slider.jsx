cac2121fb7983ca4184d7c557f2cffd7f0aa2406
Fayecodeur <fayecodeur99@gmail.com>
2025-03-04 18:27:59 +0000

    amelioration du code

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,14 +1,12 @@
-import { useEffect, useRef, useState } from "react";
-import { MdArrowBackIosNew, MdArrowForwardIos } from "react-icons/md";
-
+import { useEffect, useState, useRef } from "react";
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 import CallApi from "../Services/CallApi";
 
 const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/original";
-const widthSlider = window.innerWidth;
 
 export default function Slider() {
   const [moviesList, setMoviesList] = useState([]);
-
+  const [currentIndex, setCurrentIndex] = useState(0); // Gère l'indice de l'image actuelle
   const imageSliderRef = useRef();
 
   useEffect(() => {
@@ -17,53 +15,65 @@ export default function Slider() {
 
   const getMovies = () => {
     CallApi.getVideos.then((resp) => {
-      // console.log(resp.data.results);
       setMoviesList(resp.data.results);
     });
   };
 
-  console.log(moviesList);
-
-  const sliderRight = (el) => {
-    el.scrollLeft += widthSlider - 110;
+  const sliderRight = () => {
+    if (currentIndex < moviesList.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+    } else {
+      setCurrentIndex(0); // Recommence depuis la première image quand on atteint la fin
+    }
   };
-  const sliderLeft = (el) => {
-    el.scrollLeft -= widthSlider - 110;
+
+  const sliderLeft = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
+    } else {
+      setCurrentIndex(moviesList.length - 1); // Reviens à la dernière image si on est au début
+    }
   };
 
   return (
-    <>
-      <div className="relative z-[60]">
-        <MdArrowBackIosNew
-          onClick={() => sliderLeft(imageSliderRef.current)}
-          className="hidden md:block text-white text-[25px] absolute mx-8 mt-[250px] cursor-pointer z-50"
+    <section>
+      <div className="relative">
+        {/* Flèche gauche */}
+        <MdKeyboardArrowLeft
+          onClick={sliderLeft}
+          className="text-white text-[40px] absolute mx-8 mt-[250px] z-50 cursor-pointer"
         />
-        <MdArrowForwardIos
-          onClick={() => sliderRight(imageSliderRef.current)}
-          className="hidden md:block text-white text-[25px] absolute mx-8 mt-[250px] cursor-pointer right-0  z-50"
+        {/* Flèche droite */}
+        <MdKeyboardArrowRight
+          onClick={sliderRight}
+          className="text-white text-[40px] absolute mx-8 mt-[250px] z-50 cursor-pointer right-0"
         />
-
-        <div
-          className="  flex overflow-x-auto w-full px-16 py-4 scrollbar-hide scroll-smooth"
-          ref={imageSliderRef}
-        >
-          {moviesList.map((item, index) => (
-            <div
-              key={index}
-              className="relative min-w-full md:h-[500px] mr-5 rounded-md hover:border-[4px] border-gray-200 cursor-pointer transition-all"
-            >
-              <img
-                className="w-full h-full object-cover object-left-top  "
-                src={`${IMAGE_BASE_URL}${item.backdrop_path}`}
-                alt="Image film"
-              />
-              <h2 className="font-semibold text-white text-4xl absolute bottom-[2rem] left-[2rem]">
-                {item.name ? item.name : item.title}
-              </h2>
-            </div>
-          ))}
-        </div>
       </div>
-    </>
+
+      {/* Conteneur des images */}
+      <div
+        className="flex overflow-x-hidden w-full px-16 py-4 scrollbar-hide scroll-smooth"
+        ref={imageSliderRef}
+        style={{ height: "500px" }}
+      >
+        {moviesList.map((item, index) => (
+          <div
+            key={index}
+            className={`relative w-full flex-shrink-0 h-full mr-5 rounded-md hover:border-[4px] border-gray-200 cursor-pointer transition-all ${
+              index === currentIndex ? "block" : "hidden"
+            }`} // Affiche uniquement l'image correspondant à currentIndex
+          >
+            <img
+              src={`${IMAGE_BASE_URL}${item.backdrop_path}`}
+              alt="image film"
+              className="w-full h-full object-conver"
+            />
+            <h2 className="font-semibold text-white text-4xl absolute bottom-[2rem] left-[2rem]">
+              {item.name ? item.name : item.title}
+            </h2>
+          </div>
+        ))}
+      </div>
+    </section>
   );
 }
